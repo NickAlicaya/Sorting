14bbeb5bc69bcefa2832e5e3335c102c32437d74
nicanor-alicaya <donalicaya@gmail.com>
2020-04-15 00:03:38 -0400

    day 2 mvp attempted linear search

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -3,16 +3,17 @@ def selection_sort( arr ):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
         cur_index = i
-        smallest_index = cur_index
+        smallest_index = cur_index       
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-        for x in range(smallest_index+1,len(arr)):
+        for x in range(i+1,len(arr)):    
             if arr[x]<arr[smallest_index]:
                 smallest_index = x
         # TO-DO: swap
         if smallest_index != i:
             arr[i],arr[smallest_index]=arr[smallest_index],arr[i]
     return arr
+# print(selection_sort([3,4,1,5,6,6,4,3,2,8,0,9,8]))
 
 
 # TO-DO:  implement the Bubble Sort function below
--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,17 +1,35 @@
-# TO-DO: complete the helpe function below to merge 2 sorted arrays
+# TO-DO: complete the helper function below to merge 2 sorted arrays
 def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
-    merged_arr = [0] * elements
+    merged_arr = []
     # TO-DO
+    while len(arrA) > 0 and len(arrB) > 0:
+        if arrA[0] < arrB[0]:
+            merged_arr.append(arrA[0])
+            arrA.remove(arrA[0])
+        elif arrB[0] <= arrA[0]:
+            merged_arr.append(arrB[0])
+            arrB.remove(arrB[0])
+    while len(arrA) > 0:
+        merged_arr.append(arrA[0])
+        arrA.remove(arrA[0])
+    while len(arrB) > 0:
+        merged_arr.append(arrB[0])
+        arrB.remove(arrB[0])
     
     return merged_arr
-
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     # TO-DO
+    length = len(arr)
+    if length <= 1:
+        return arr
+    x = length//2
+    arrA = merge_sort(arr[x:])
+    arrB = merge_sort(arr[:x])
 
-    return arr
+    return merge(arrA,arrB)
 
 
 # STRETCH: implement an in-place merge sort algorithm
